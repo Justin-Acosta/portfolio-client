6bea0d374abb852fe9b1275bcabd4d5e7b493f70
Justin-Acosta <jacostaprof@gmail.com>
2025-02-10 18:45:51 -0600

    added mobile first and nav bar sections

--- app/responsive-design/page.tsx
+++ app/responsive-design/page.tsx
@@ -2,14 +2,28 @@
 export default function Page() {
     return (
       <main>
+
         <h1>Responsive Design</h1>
+
         <section>
           <h1>Mobile First</h1>
           <p>I'm designing this website to be viewed on a mobile device,
             but as a self proclaimed desktop power user, I need to show
-            some love 
+            some love to that sweet 16/9. I decided to transition formats
+            based on aspect ratio, using 4/4 as my threshold because few
+            displays are ever a perfect square.
           </p>
         </section>
+
+        <section>
+          <h1>Nav Bar</h1>
+          <p>I've used Tailwind and Bootstrap to make responsive nav bars,
+            before but I've always wanted to create one on my own. Change
+            the screen to a mobile format, and you'll see the fruits of
+            my self imposed challenge yourself.
+          </p>
+        </section>
+
       </main>
     );
   }
